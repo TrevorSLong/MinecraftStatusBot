7ec4908b8b8e6c524094c944207fc53a9f4f7258
Trevor Long <longtrev12@gmail.com>
2022-01-19 09:47:04 -0700

    Update MinecraftStatusBot.py

--- MinecraftStatusBot.py
+++ MinecraftStatusBot.py
@@ -48,30 +48,23 @@ status = server.status()
 print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
 
 ##############Changes bot status (working)###########################################################################################
-
 class MyCog(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self):
         self.index = 0
-        self.bot = bot
         self.printer.start()
 
     def cog_unload(self):
         self.printer.cancel()
 
-    @tasks.loop(seconds=60.0)
+    @tasks.loop(seconds=5.0)
     async def printer(self):
         print(self.index)
         self.index += 1
+        
         server = MinecraftServer.lookup(SERVER)
         status = server.status()
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="{0} players play online!".format(status.players.online, status.latency)))
         
-
-    @printer.before_loop
-    async def before_printer(self):
-        print('waiting...')
-        await self.bot.wait_until_ready()
-  
 ##############Reponds to ping (working)########################################################################################################
 @slash.slash(
 	description="Responds with Pong and the bots server latency", 	# ADDS THIS VALUE TO THE $HELP PING MESSAGE.
